3a1c920227df49e531835a2e607af935d817935a
Mikias Goitom <mikiasgoitom7@gmail.com>
2024-12-30 10:47:40 +0300

    Added route to map, updated sidebar icons, and made map client-side rendered
    - Added route rendering to the map component
    - Wrapped sidebar icons in a div for better styling
    - Modified the root page to ensure the map is client-side rendered using Next.js dynamic import
    - Added a custom hook to fetch and use the user's location
    - Included new icons in the public directory for use in the application

--- ietp-frontend/next.config.ts
+++ ietp-frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+      dns: false,
+      tls: false,
+      fs: false,
+      request: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
